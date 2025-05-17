35596975dd448e4eff2c4a32f0119bc85fbe7882
Pavani <136723974+NerusuPavani37@users.noreply.github.com>
2025-05-17 14:49:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let prices = document.querySelectorAll('.price')
+ let totalPrice = 0;
+for(let i=0;i<prices.length;i++){
+  console.log(prices[i].textContent);
+  
+  totalPrice +=Number(prices[i].textContent);
+}
+console.log(totalPrice);
   
 };
 
